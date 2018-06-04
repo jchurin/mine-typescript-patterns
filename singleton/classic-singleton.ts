2d2952f2c2413e42c7fef7b423b63803a6a2d32b
jchurin95 <juanchurin@gmail.com>
2018-06-04 09:21:03 -0300

    First commit

---
+++ singleton/classic-singleton.ts
@@ -0,0 +1,16 @@
+export class ClassicSingleton {
+
+    private static instance: ClassicSingleton = null;
+
+    protected constructor() {
+        // Exists only to defeat instantiation.
+    }
+
+    public static getInstance(): ClassicSingleton {
+        if (this.instance === null) {
+            this.instance = new ClassicSingleton(); 
+        }
+        return this.instance;
+    }
+
+}
